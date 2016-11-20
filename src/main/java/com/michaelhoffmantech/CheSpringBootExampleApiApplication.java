ee4d820a17daa4f9f17002b269ff74ee64251346
Michael Hoffman <mike@michaelhoffmaninc.com>
2016-11-20 10:25:00 -0600

    Initial commit

---
+++ src/main/java/com/michaelhoffmantech/CheSpringBootExampleApiApplication.java
@@ -0,0 +1,12 @@
+package com.michaelhoffmantech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheSpringBootExampleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheSpringBootExampleApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/michaelhoffmantech/CheSpringBootExampleApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.michaelhoffmantech;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CheSpringBootExampleApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
